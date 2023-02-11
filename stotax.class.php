a7854b8bf91500b6521692fd2a49ffffd289f7c7
Florian Wiessner <f.wiessner@smart-weblications.de>
2023-02-11 23:18:29 +0100

    Update stotax.class.pp
    Update because of stotax-online.de UI Update

--- stotax.class.php
+++ stotax.class.php
@@ -2,6 +2,7 @@
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverDimension;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use GuzzleHttp\Client;
 
 class stotax
 {
@@ -14,9 +15,9 @@ class stotax
         
         $this->cookie_file = "/tmp/cookiefile";
 
-        $browserdriver_params = ['--headless','--no-sandbox'];
+        $browserdriver_params = ['--headless','--no-sandbox','--disable-gpu'];
         if ($debug) {
-            $browserdriver_params = ['--no-sandbox'];
+            $browserdriver_params = ['--no-sandbox','--disable-gpu'];
         }
         
         
@@ -41,10 +42,10 @@ class stotax
         $this->user_info = $this->get_user_info_from_jwt($this->header["Authorization"]);
     }
 
-    function stotax_upload_file($file, $fachlichkeit = "Rewe-Buchungsbelege", $time = NULL)
-    {
+    
+    function stotax_upload_belege($files, $fachlichkeit = "Rewe-Buchungsbelege", $time = NULL) { 
         $header = $this->header;
-
+        
         $header["Sec-Fetch-Dest"] = "empty";
         $header["Sec-Fetch-Mode"] = "cors";
         $header["Sec-Fetch-Site"] = "same-origin";
@@ -54,112 +55,76 @@ class stotax
         $header["Origin"] = "https://www.stotax-online.de";
         $header["Connection"] = "keep-alive";
         $header["Accept-Encoding"] = "gzip, deflate, br";
-
+        
         $fachlichkeit_id = $this->fachlichkeiten[$fachlichkeit];
         $mandant = $this->mandant;
-        $post_url = "https://www.stotax-online.de/auswertungen/api/v1/mandant/$mandant/beleg?fachlichkeitId=$fachlichkeit_id&customSubject=&privateData=false";
-
-        $split = preg_split("/\//", $file);
-        $upload_filename = array_pop($split);
-        $cf = new CURLFile($file, mime_content_type($file), $upload_filename);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $post_url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_VERBOSE, false);
+        
+        $user_info = (array) $this->user_info ;
+        $user_identifier = $user_info["http://schemas.stotax.de/2012/02/identity/claims/useridentifier"];
+        
+        $post_url = "https://www.stotax-online.de/selectnetcore/p0/api/$user_identifier/auswertungen/createBeleg";
+        
+        
         if (isset($time)) {
             list($y,$m,$d) = preg_split("/-/",$time);
-            $fachlichkeitsZeitraumVon = date("D M d Y", mktime(0,0,0,$m,$d,$y));
+            $fachlichkeitsZeitraumVon = date("Y-m-d", mktime(0,0,0,$m,$d,$y)) . "T00:00:00";
         } else {
             $fachlichkeitsZeitraumVon = 'null';
         }
-        curl_setopt($ch, CURLOPT_POSTFIELDS, [
-            // ATTACH FILE UPLOAD
-            "file" => $cf,
-            "fachlichkeitsZeitraumVon" => $fachlichkeitsZeitraumVon,
-            "text" => 'Autouploaded',
-            "zeitraumTyp" => 8
-        ]);
-
-        foreach ($header as $k => $v) {
-            $curl_header[] = "$k: $v";
-        }
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_header);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            return "CURL ERROR - " . curl_error($ch);
-        } 
-        else {
-            return $result;
-        }
-    }
-
-    function stotax_upload_files($files, $fachlichkeit = "Rewe-Buchungsbelege", $time = NULL)
-    {
-        $header = $this->header;
         
-        $header["Sec-Fetch-Dest"] = "empty";
-        $header["Sec-Fetch-Mode"] = "cors";
-        $header["Sec-Fetch-Site"] = "same-origin";
-        $header["TE"] = "trailers";
-        $header["Referer"] = "https://www.stotax-online.de/auswertungen/";
-        $header["Host"] = "www.stotax-online.de";
-        $header["Origin"] = "https://www.stotax-online.de";
-        $header["Connection"] = "keep-alive";
-        $header["Accept-Encoding"] = "gzip, deflate, br";
         
-        $fachlichkeit_id = $this->fachlichkeiten[$fachlichkeit];
-        $mandant = $this->mandant;
-        $post_url = "https://www.stotax-online.de/auswertungen/api/v1/mandant/$mandant/beleg?fachlichkeitId=$fachlichkeit_id&customSubject=&privateData=false";
+        $fileObjects = array();
         
-        foreach ($files as $index => $file) { 
-            $postData['file[' . $index . ']'] = curl_file_create(
-                realpath($file),
-                mime_content_type($file),
-                basename($file)
-                );
+        
+        foreach ($files as $index => $file) {
             
-        }
-
-        if (isset($time)) {
-            list($y,$m,$d) = preg_split("/-/",$time);
-            $fachlichkeitsZeitraumVon = date("D M d Y", mktime(0,0,0,$m,$d,$y));
-        } else {
-            $fachlichkeitsZeitraumVon = 'null';
+            $file_base = basename($file);
+            
+            $fileObjects[$file_base] = ["id" => "00000000-0000-0000-0000-000000000000", "content" => base64_encode(file_get_contents($file)), "extension" => pathinfo($file, PATHINFO_EXTENSION) , "mimeType" => mime_content_type($file), "dokuments" => array(), "kundenpostfachDokuments" => array() ];
         }
         
-        $postData["fachlichkeitsZeitraumVon"] = $fachlichkeitsZeitraumVon; 
-        $postData["text"] = "Autouploaded";
-        $postData["zeitraumTyp"] = 8 ; 
         
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $post_url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_VERBOSE, false);
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+         
         
-        foreach ($header as $k => $v) {
-            $curl_header[] = "$k: $v";
-        }
+        $data = ["text" => "autoupload", "nachricht" => "autoupload", "fileObjects" => $fileObjects , "useSubjectForEmail" => false, "private" => false, "synchronizationOptions" => 0 , "fachlichkeitsDatumVonL" => $fachlichkeitsZeitraumVon, "mandantId" => "00000000-0000-0000-0000-000000000000", "fachlichkeitId" => $fachlichkeit_id, "einstellVorgangTyp" => 1, "zeitraumTypen" => 8, "antwortId" => null, "stotaxId" => null, "kanzleiIdentifier" => null] ;
         
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_header);
         
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            return "CURL ERROR - " . curl_error($ch);
-        }
-        else {
-
-            return $result;
-        }
+        $gclient = new Client();
+        
+        $header = $this->header;
+        
+        $header["Sec-Fetch-Dest"] = "empty";
+        $header["Sec-Fetch-Mode"] = "cors";
+        $header["Sec-Fetch-Site"] = "same-origin";
+        $header["TE"] = "trailers";
+        $header["Referer"] = "https://www.stotax-online.de/auswertungen/";
+        $header["Host"] = "www.stotax-online.de";
+        $header["Origin"] = "https://www.stotax-online.de";
+        $header["Connection"] = "keep-alive";
+        $header["Accept-Encoding"] = "gzip, deflate, br";
+        $header["Accept"] = "application/json, text/plain, */*";
+        $header["content-type"] = "application/json; charset=utf-8";
+        
+       
+        
+        
+        $response = $gclient->request('PUT',$post_url, [
+            'headers' => $header,
+            'body' => json_encode($data),
+            
+        ]);
+        
+        
+        $data = $response->getBody()->getContents() ;
+        
+        
+        return $data;
+        
+        
     }
     
     
+   
     
     function stotax_download_files($save_dir = null)
     {
@@ -175,23 +140,86 @@ class stotax
         $this->client->waitForVisibility('.eingaenge');
         $this->client->executeScript("document.querySelector('li[title=\'Anwendungsauswahl\'] span[class=\'ng-binding\']').click();");
         $this->client->clicklink('Eingänge (Auswertungen)');
-        $this->client->waitForVisibility('.downloadLink');
-        $crawler = $this->client->refreshCrawler();
- 
-        $list = $crawler->filterXPath('//table//tr[starts-with(@id, "item_")]');
-       
-        foreach ($list as $trnode) { 
-            $item_id = $trnode->getAttribute('id');
-            $item_ids[] = $item_id; 
-            $filenames[$item_id] = $crawler->filterXPath("//tr[@id='$item_id']//div[@class='downloadLink ng-binding']")->text();
-        }
-        array_unique($item_ids);
-        array_unique($filenames);
-        $downloaded = array();
 
-        foreach ($item_ids as $document_id) { 
+        $crawler = $this->client->refreshCrawler();
+        
+        $user_info = (array) $this->user_info ; 
+        $user_identifier = $user_info["http://schemas.stotax.de/2012/02/identity/claims/useridentifier"];
+        
+        $vorgaenge_url = "https://www.stotax-online.de/selectnetcore/p0/api/$user_identifier/auswertungen/einstellVorgaenge";
+        
+       
+        
+        $data_raw = '{"baseData":{"pageSize":1000,"currentPage":1,"dateFrom":null,"dateTo":null,"searchText":null,"filterFachlichkeitId":"92689686-837e-e311-98be-00ff678cb26f","paginationEntityId":null,"paginationEntityType":0,"includeGestricheneBewegung":false,"vorgangsFilterTyp":0},"mitAntwortInfo":true}';
+        $data_json = json_decode($data_raw);
+        
+       
+        $header = $this->header;
+        
+        $header["Sec-Fetch-Dest"] = "empty";
+        $header["Sec-Fetch-Mode"] = "cors";
+        $header["Sec-Fetch-Site"] = "same-origin";
+        $header["TE"] = "trailers";
+        $header["Referer"] = "https://www.stotax-online.de/auswertungen/";
+        $header["Host"] = "www.stotax-online.de";
+        $header["Origin"] = "https://www.stotax-online.de";
+        $header["Connection"] = "keep-alive";
+        $header["Accept-Encoding"] = "gzip, deflate, br";
+        $header["Accept"] = "application/json, text/plain, */*";
+        $header["content-type"] = "application/json; charset=utf-8";
+        
+     
+        
+        $gclient = new Client();
+        
+        $response = $gclient->request('POST',$vorgaenge_url, [
+            'headers' => $header,
+            'body' => json_encode($data_json), 
+            
+        ]);
+        
+        
+        $data = $response->getBody()->getContents() ; 
+        $data_json = json_decode($data);
+        //var_dump($data_json);
+        
+        $einstellVorgaenge = $data_json->einstellVorgaenge ; 
+        
+        foreach ($einstellVorgaenge as $vorgang) { 
+            foreach ($vorgang->dokuments->{'$values'} as $doc) { 
+                
+                $to_download[$doc->id] = $doc->name ; 
+            }
+        }
+        
+        
+        
+    
+        
+        
+        // fuer jedes document getott machen um den onetimekey zu holen: 
+        
+        // https://www.stotax-online.de/selectnetcore/p0/api/$user_identifier/download/getott
+        
+        // payload: {"target":"https://www.stotax-online.de/selectnetcore/p0/api/f0484e49-58e4-429c-bb3e-cc0a37a4941e/download/dokumentauswertungen/8a6380df-e4a6-ed11-ad8d-0050568d5f34/972_10151_000_000000_202301_20230207_131546_LStA-Druck.PDF"}
+        $downloaded = array();
+        
+        foreach ($to_download as $id => $name) { 
+            
+            $document_id = $id; 
             
             
+            $filename = "item_". $document_id."_".$name;
+            if (file_exists($save_dir."/".$filename)) {
+                $downloaded[] = "skipped: ".$save_dir."/".$filename ;
+                continue;
+            }
+            
+            $ott = $this->getott($id,$name);
+            $url_fname = str_replace(" ", "", $name);
+            $url = "https://www.stotax-online.de/selectnetcore/p0/api/$user_identifier/download/dokumentauswertungen/$id/$url_fname?onetimetokenid=$ott";
+            
+            //echo $url ;
             $header = $this->header;
             
             $header["Sec-Fetch-Dest"] = "empty";
@@ -205,41 +233,8 @@ class stotax
             $header["Accept-Encoding"] = "gzip, deflate, br";
             $header["Accept"] = "application/json, text/plain, */*";
             
-            $filename = $document_id."_".$filenames[$document_id];
-            if (file_exists($save_dir."/".$filename)) { 
-                $downloaded[] = "skipped: ".$save_dir."/".$filename ; 
-                continue; 
-            }
-            $document_id = str_replace("item_","",$document_id);
-            $get_url = "https://www.stotax-online.de/auswertungen/api/v1/accesstoken?baseString=api%2Fv1%2Fdokument%2F$document_id%257CDokument%257C$document_id";
-            
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $get_url);
-            curl_setopt($ch, CURLOPT_VERBOSE, false);
-             
-            foreach ($header as $k => $v) {
-                $curl_header[] = "$k: $v";
-            }
-            
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_header);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($ch);
-            if (curl_errno($ch)) {
-                return "CURL ERROR - " . curl_error($ch);
-            }
-            else {
-                $token_result = json_decode($result);
-            }
-            $accessToken = $token_result->accessToken; 
-            
-            unset($curl_header);
-            unset($header["Authorization"]);
-            
-            //get the file and save it
-            $get_url = "https://www.stotax-online.de/auswertungen/api/v1/dokument/$document_id?accessKey=$accessToken";
-            
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $get_url);
+            curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_VERBOSE, false);
             
             foreach ($header as $k => $v) {
@@ -252,12 +247,17 @@ class stotax
             if (curl_errno($ch)) {
                 return "CURL ERROR - " . curl_error($ch);
             }
-
+            
             $fh = fopen($save_dir."/".$filename,"a");
             fputs($fh,$result,strlen($result));
             fclose($fh);
             $downloaded[] = "downloaded: ". $save_dir."/".$filename ; 
+            
+            unset($header);
+            unset($curl_header);
         }
+        
+
         return $downloaded; 
         
     }
@@ -296,6 +296,7 @@ class stotax
                 $header = (array) $header;
 
                 if (isset($header["Authorization"])) { 
+
                     break;
                 }
             }
@@ -341,6 +342,47 @@ class stotax
     
     function base64url_decode($data) { 
         return base64_decode( strtr( $data, '-_', '+/') . str_repeat('=', 3 - ( 3 + strlen( $data )) % 4 ));
+    }
+    
+    function getott($id,$name) {
+        
+        $name = str_replace(" ", "", $name);
+        $user_info = (array) $this->user_info ;
+        $user_identifier = $user_info["http://schemas.stotax.de/2012/02/identity/claims/useridentifier"];
+        
+        $url = "https://www.stotax-online.de/selectnetcore/p0/api/$user_identifier/download/getott";
+        
+        $gclient = new Client();
+        
+        $header = $this->header;
+        
+        $header["Sec-Fetch-Dest"] = "empty";
+        $header["Sec-Fetch-Mode"] = "cors";
+        $header["Sec-Fetch-Site"] = "same-origin";
+        $header["TE"] = "trailers";
+        $header["Referer"] = "https://www.stotax-online.de/auswertungen/";
+        $header["Host"] = "www.stotax-online.de";
+        $header["Origin"] = "https://www.stotax-online.de";
+        $header["Connection"] = "keep-alive";
+        $header["Accept-Encoding"] = "gzip, deflate, br";
+        $header["Accept"] = "application/json, text/plain, */*";
+        $header["content-type"] = "application/json; charset=utf-8";
+        
+        $payload = '{"target":"https://www.stotax-online.de/selectnetcore/p0/api/'.$user_identifier.'/download/dokumentauswertungen/'.$id.'/'.$name.'"}';
+       
+        
+        $response = $gclient->request('POST',$url, [
+            'headers' => $header,
+            'body' => ($payload),
+            
+        ]);
+        
+        
+        $data = $response->getBody()->getContents() ;
+        $data_json = json_decode($data);
+
+        
+        return $data_json->content;
     }
     
 }
